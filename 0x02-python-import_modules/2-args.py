be973a9832345439f8aa82d4b953707700e830c4
daniapm <daniajibellis@hotmail.com>
2021-04-28 21:04:52 +0000

    updating files

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 import sys
 
-parametros = len(sys.argv) - 1
-if len(sys.argv) - 1 == 0:
-    print("0 arguments.")
-elif len(sys.argv) - 1 == 1:
-    print("1 argument:")
-    print("1: {}".format(sys.argv[1]))
-else:
-    print("{} arguments:".format(parametros))
-    for count in range(1, parametros + 1):
-        print("{}: {}".format(count, sys.argv[count]))
+
+def main():
+    parametros = len(sys.argv) - 1
+    if len(sys.argv) - 1 == 0:
+        print("0 arguments.")
+    elif len(sys.argv) - 1 == 1:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(parametros))
+        for count in range(1, parametros + 1):
+            print("{}: {}".format(count, sys.argv[count]))
+
+if __name__ == "__main__":
+    main()
