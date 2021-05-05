11c90e236110e407013930bd70f3d2a0fd22c360
daniapm <daniajibellis@hotmail.com>
2021-05-05 22:54:12 +0000

    replaces an element of a list at a specific position

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0:
-        return my_list
-    elif idx > len(my_list):
-        return my_list
-    else:
-        my_list[idx] = element
-        return my_list
+    if (my_list is None) or (idx is None):
+        return
+    elif (idx < 0) or (idx >= len(my_list)):
+        return
+    my_list[idx] = element
+    return my_list
