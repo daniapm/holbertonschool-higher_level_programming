6c4083070482efd58d0bd599ef5ed6a333f2a918
Dania Puertas <daniajibellis@hotmail.com>
2021-05-18 21:43:32 +0000

    Area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+
+"""
+definition square class
+
+"""
+
+
+class Square:
+
+    """
+    class Private instance attribute: size
+
+    """
+    def __init__(self, size=0):
+        """
+        argc:
+        size: Square size
+
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        argc:
+        self: parametro
+        Returns:
+        area: square area
+
+        """
+        area = self.__size * self.__size
+        return area
