1343114ca5faf5b2dd8cd5fa1c3a51dde8bca5c8
Dania Puertas <daniajibellis@hotmail.com>
2021-05-18 22:14:50 +0000

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+
+"""
+definition square class
+
+"""
+
+
+class Square:
+
+    """
+    class Private instance attribute: size
+
+    """
+    def __init__(self, size=0):
+        """
+        argc:
+        size: Square size
+
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        """
+        I'm the 'size' property.
+
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        argc:
+        size: Square size
+
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        argc:
+        self: parametro
+        Returns:
+        area: square area
+
+        """
+        area = self.__size * self.__size
+        return area
