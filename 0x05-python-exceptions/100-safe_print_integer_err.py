d746d2bf98e8d0e7f9ba1cdc066c7ce2ca9e73e1
Dania Puertas <daniajibellis@hotmail.com>
2021-05-18 03:00:38 +0000

    stderr imprimiendo error

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except (TypeError, ValueError) as exc:
+        sys.stderr.write("Exception: {}\n".format(exc))
+        return(False)
+    else:
+        return(True)
