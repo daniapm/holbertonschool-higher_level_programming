045406106e2780be7f89fd16fad2a8bf732495c3
Dania Puertas <daniajibellis@hotmail.com>
2021-05-19 04:11:45 +0000

    Coordinates of a square

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+
+"""
+definition square class
+
+"""
+
+
+class Square:
+
+    """
+    class Private instance attribute: size
+
+    """
+    def __init__(self, size=0):
+        """
+        argc:
+        size: Square size
+
+        """
+        if type(size) is not int or type(size) is not float:
+            raise TypeError("size must be a number")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        """
+        I'm the 'size' property.
+
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        argc:
+        size: Square size
+
+        """
+        if type(size) is not int or type(size) is not float:
+            raise TypeError("size must be a number")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        argc:
+        self: parametro
+        Returns:
+        area: square area
+
+        """
+        area = self.__size * self.__size
+        return area
+
+    def __eq__(self, other):
+        return self.__size == other.__size
+
+    def __lt__(self, other):
+        return self.__size == other.__size
