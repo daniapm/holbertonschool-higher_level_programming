321acfb3255faf659d98c88e13be69fb5f64e0ca
Dania Puertas <daniajibellis@hotmail.com>
2021-05-20 23:56:13 +0000

    se crea metodo get y setter

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -31,3 +31,47 @@ class Rectangle:
                 raise ValueError("width must be >= 0")
             else:
                 self.__width = width
+        
+        @property
+        def height(self):
+            """
+            I'm the 'height' property.
+
+            """
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            """
+            argc:
+            height: Rectangle height
+
+            """
+            if type(value) is not int:
+                    raise TypeError("height must be an integer")
+            elif value < 0:
+                raise ValueError("height must be >= 0")
+            else:
+                self.__height = value
+
+        @property
+        def width(self):
+            """
+            I'm the 'width' property.
+
+            """
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            """
+            argc:
+            width: Rectangle width
+
+            """
+            if type(value) is not int:
+                raise TypeError("width must be an integer")
+            elif value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
