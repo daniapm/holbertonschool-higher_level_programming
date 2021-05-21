d2a531b9ed71ff466d09331a5b63e357eb9798ae
Dania Puertas <daniajibellis@hotmail.com>
2021-05-21 02:09:53 +0000

    corrigiendo condicionales 3

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -148,16 +148,13 @@ class Rectangle:
         Return: comparation of area of the rectangules
 
             """
-
+            if not isinstance(rect_1, Rectangle):
+                raise TypeError("rect_1 must be an instance of Rectangle")
+            if not isinstance(rect_2, Rectangle):
+                raise TypeError("rect_2 must be an instance of Rectangle")
             if rect_1.area() > rect_2.area():
                 return rect_1
-            elif rect_2.area() == rect_1.area():
-                return rect_1
-            else:
+            elif rect_2.area() > rect_1.area():
                 return rect_2
-            x = isintance(rect_1, Rectangle)
-            if x is not instance:
-                raise TypeError("rect_1 must be an instance of Rectangle")
-            y = isintance(rect_2, Rectangle)
-            if y is not instance:
-                raise TypeError("rect_2 must be an instance of Rectangle")
+            else:
+                return rect_1
