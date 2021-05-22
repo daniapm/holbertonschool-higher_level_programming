a863c102957eea77e19c7d822aa0d2e35105be2f
Dania Puertas <daniajibellis@hotmail.com>
2021-05-22 03:25:57 +0000

    Square instance can answer to comparators

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -64,3 +64,15 @@ class Square:
 
     def __lt__(self, other):
         return self.__size == other.__size
+
+    def __gt__(self, other):
+        return self.__size > other.__size
+
+    def __ge__(self, other):
+        return self.__size >= other.__size
+
+    def __le__(self, other):
+        return self.__size <= other.__size
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
