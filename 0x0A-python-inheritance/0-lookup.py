a0528fa834d959decdef9d12357529ae26c797f3
Dania Puertas <daniajibellis@hotmail.com>
2021-06-02 00:22:39 +0000

    adding documentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+"""
+Documentation for MyList class that inherits from the list class
+"""
+
+
 def lookup(obj):
+    """
+    returns the list of available attributes and methods of an object
+    argc:
+    obj: the object is an instance of a class that inherited
+    Return: dir of the obj
+    """
     return (dir(obj))
