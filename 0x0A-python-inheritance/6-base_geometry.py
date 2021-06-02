cafc71d3081ecddfac27c72468f17f96b2be0f0d
Dania Puertas <daniajibellis@hotmail.com>
2021-06-02 00:33:14 +0000

    file class BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Documentation for BaseGeometty class empy
+"""
+
+
+class BaseGeometry():
+    """
+    BaseGeometty class
+    """
+    def area(self):
+        """
+        Public instance method
+        Return: Exception with the message
+        """
+        raise Exception("area() is not implemented")
