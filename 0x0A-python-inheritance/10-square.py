546a7fc3de4f6e9a934b9b6f84b45a7931903554
Dania Puertas <daniajibellis@hotmail.com>
2021-06-02 02:59:51 +0000

    correcting syntax

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -27,6 +27,10 @@ class BaseGeometry():
             raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+"""
+Documentation for Rectangle class
+"""
+
 
 class Rectangle(BaseGeometry):
     """
