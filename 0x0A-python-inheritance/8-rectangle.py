9cdd0235e3fd9406872b3e3b883679d4ee953ac6
Dania Puertas <daniajibellis@hotmail.com>
2021-06-02 02:32:31 +0000

    8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -24,9 +24,8 @@ class BaseGeometry:
         """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-
 
 """
 Documentation for BaseGeometty class empy
