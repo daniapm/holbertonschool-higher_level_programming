e7d6e0d36840760f7c7195bf87faf4a55f0f078e
Dania Puertas <daniajibellis@hotmail.com>
2021-06-02 02:51:55 +0000

    correcting syntax

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,69 +1,6 @@
 #!/usr/bin/python3
-"""
-Documentation for BaseGeometty class empy
-"""
 
-
-class BaseGeometry():
-    """
-    BaseGeometty class
-    """
-    def area(self):
-        """
-        Public instance method area
-        Return: Exception with message
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Public instance method integer_validator
-        argc:
-        name: always a string
-        value: validates value
-        Return: Exception with message
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-"""
-Documentation for Rectangle class
-"""
-
-
-class Rectangle(BaseGeometry):
-    """
-    Rectangle class
-    """
-    def __init__(self, width, height):
-        """
-        Instantiation with width and heigth
-        argc:
-        width: width of the Rectangule
-        height: heigth of the Rectangule
-        """
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        """
-        Public instance method area
-        Return: Regtangule area
-        """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """
-        Public instance method str
-        Return: str area
-        """
-        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
-
+Rectangle = __import__('9-rectangle').Rectangle
 
 """
 Documentation for Square class
@@ -81,6 +18,7 @@ class Square(Rectangle):
         size: size of the Square
         """
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
@@ -88,11 +26,4 @@ class Square(Rectangle):
         Public instance method area
         Return: Regtangule area
         """
-        return self.__size * self.__size
-
-    def __str__(self):
-        """
-        Public instance method str
-        Return: str area
-        """
-        return ("[Rectangle] {}/{}".format(self.__size, self.__size))
+        return self.__size ** 2
