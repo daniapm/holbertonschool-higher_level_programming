cc1f2a5039fbebca96dce5231d1d7ac1202ba1fc
Dania Puertas <daniajibellis@hotmail.com>
2021-06-03 01:34:27 +0000

    file readme modified

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,5 +2,5 @@
 def read_file(filename=""):
     """Read a plain text file and return the contents as a string."""
     with open(filename, encoding='utf-8') as file:
-        read_data = file.read()
-        print(read_data, end="")
+        for i in file:
+            print(i, end="")
