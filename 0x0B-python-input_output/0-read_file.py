3944b2b265227d96330ec90851fff28a2dee9abb
Dania Puertas <daniajibellis@hotmail.com>
2021-06-03 01:36:24 +0000

    agg documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Documentation for read_file function"""
+
+
 def read_file(filename=""):
-    """Read a plain text file and return the contents as a string."""
+    """Read a plain text file and return the contents as a string.
+    Args:
+        filename: file to open
+    """
     with open(filename, encoding='utf-8') as file:
-        for i in file:
-            print(i, end="")
+        read_data = file.read()
+        print(read_data, end="")
