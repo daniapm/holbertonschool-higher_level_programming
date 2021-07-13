82372f3cc2e111e3814f9edfe28aaf78669a7caf
daniapm <daniajibellis@hotmail.com>
2021-07-13 16:58:28 -0500

    adding asc

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- script that lists all the cities of California that can be found in the database hbtn_0d_usa.
 SELECT cities.id, cities.name FROM states, cities
-WHERE states.name = 'California' AND states.id = states.id
-ORDER BY cities.id DESC;
+WHERE states.name = 'California' AND states.cities = states.id
+ORDER BY cities.id ASC;
