5989ec4fc36c2b8e8b9087859e7dc2dcfe506e75
daniapm <daniajibellis@hotmail.com>
2021-07-13 16:30:10 -0500

    changing syntax

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- script that lists all the cities of California that can be found in the database hbtn_0d_usa.
 SELECT cities.id, cities.name FROM states, cities
-WHERE states.name = 'California' AND cities.states.id = state_id
+WHERE states.name = 'California' AND states.id = states.id
 ORDER BY cities.id DESC;
