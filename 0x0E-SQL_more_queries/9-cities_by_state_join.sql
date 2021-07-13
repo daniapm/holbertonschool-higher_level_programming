8f6308e3186ff12eb8d992a1e4e6fe7d1e2e11af
daniapm <daniajibellis@hotmail.com>
2021-07-13 16:18:48 -0500

    Cities by States

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,4 @@
+-- script that lists all cities contained in the database hbtn_0d_usa.
+SELECT cities.id, cities.name  states.name FROM cities
+JOIN cities ON cities.state_id = state.id
+ORDER BY cities.id DESC;
