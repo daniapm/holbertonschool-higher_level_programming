afe65aa0bb9c1bac238867cff9525517347d8fdd
daniapm <daniajibellis@hotmail.com>
2021-07-13 21:23:31 -0500

    genered id show

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,6 +3,10 @@ SELECT tv_shows.name
     FROM tv_genres
     JOIN tv_show_genres
     ON tv_show_genres.genre_id = tv_genres.id
+
+    JOIN tv_shows
+    ON tv_show_genres.show_id = tv_shows.id
+    WHERE tv_shows.title = 'Dexter'
 GROUP BY tv_show_genres.genre_id
 ORDER BY tv_genres.name
 ASC;
