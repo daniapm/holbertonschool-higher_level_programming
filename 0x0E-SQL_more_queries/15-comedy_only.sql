984fb732ad31d95822673b3ca56af0c5f50828d6
daniapm <daniajibellis@hotmail.com>
2021-07-13 21:39:18 -0500

    changed sintax

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,11 +1,11 @@
 -- script that lists all Comedy shows in the database hbtn_0d_tvshows.
 SELECT tv_shows.title
-    FROM tv_genres
+    FROM tv_shows
     JOIN tv_show_genres
-    ON tv_show_genres.genre_id = tv_genres.id
-
-    JOIN tv_shows
     ON tv_show_genres.show_id = tv_shows.id
-    WHERE tv_genres.name = Comedy
+
+    JOIN tv_genres
+    ON tv_show_genres.genre_id = tv_genres.id 
+    WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title
 ASC;
