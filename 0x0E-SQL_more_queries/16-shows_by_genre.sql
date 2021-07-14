a01012cfbda2cbc57e87220c25e5757fff48a5a9
daniapm <daniajibellis@hotmail.com>
2021-07-13 21:55:12 -0500

    shows by genre

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,9 +2,9 @@
 SELECT tv_shows.title, tv_genres.name
     FROM tv_shows
     JOIN tv_show_genres
-    ON tv_show_genres.show_id = tv_shows.id
+    ON tv_show_genres.genre_id = tv_genres.id
 
     JOIN tv_genres
-    ON tv_show_genres.genre_id = tv_genres.id
+    ON tv_show_genres.show_id = tv_shows.id 
 ORDER BY tv_shows.title, tv_genres.name
 ASC;
