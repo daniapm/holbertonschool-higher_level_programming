c0f3f3aeb68e9dbd5f9cd590a35837938622f043
daniapm <daniajibellis@hotmail.com>
2021-07-13 21:26:05 -0500

    genered id show

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
-SELECT tv_shows.name
+SELECT tv_genres.name
     FROM tv_genres
     JOIN tv_show_genres
     ON tv_show_genres.genre_id = tv_genres.id
@@ -7,6 +7,5 @@ SELECT tv_shows.name
     JOIN tv_shows
     ON tv_show_genres.show_id = tv_shows.id
     WHERE tv_shows.title = 'Dexter'
-GROUP BY tv_show_genres.genre_id
 ORDER BY tv_genres.name
 ASC;
