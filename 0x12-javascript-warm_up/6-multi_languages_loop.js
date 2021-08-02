b6e0a1e585535f26fdb68779ca4e4acd780fded6
daniapm <daniajibellis@hotmail.com>
2021-08-02 14:44:25 -0500

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// script that prints 3 lines The first line: “C is fun”
+// The second line: “Python is cool”
+// The third line: “JavaScript is amazing”
+const MyArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+MyArray.forEach(function (elemento, array) {
+  console.log(elemento);
+});
