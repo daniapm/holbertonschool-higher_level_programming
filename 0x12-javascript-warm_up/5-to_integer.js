884cf4e366b24ad3b442a41c493de04d5bcdef50
daniapm <daniajibellis@hotmail.com>
2021-08-02 14:35:30 -0500

    file that converted argumentos to integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 // prints a message depending of the number of arguments passed
 if (process.argv[2] === undefined) {
   console.log('Not a number');
-} else if (process.argv[2] !== String) {
+} else if (isNaN(process.argv[2])) {
   console.log('Not a number');
 } else {
   console.log('My number' + ' ' + parseInt(process.argv[2]));
