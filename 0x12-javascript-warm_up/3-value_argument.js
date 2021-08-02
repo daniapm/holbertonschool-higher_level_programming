49f8bcbf55732bd7bb543bcc7e0070f9b96185c8
daniapm <daniajibellis@hotmail.com>
2021-08-02 14:11:43 -0500

    edit conditional

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // prints a message depending of the number of arguments passed
-if (process.argv[1] === '/home/daniapm/holbertonschool-higher_level_programming/0x12-javascript-warm_up/3-value_argument.js') {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
