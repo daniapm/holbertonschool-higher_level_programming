dbf3cf0c3f9624c0e346f02de2e4d28c27c70c06
daniapm <daniajibellis@hotmail.com>
2021-08-02 15:15:49 -0500

    script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,8 @@
 // script that prints 3 lines The first line: “C is fun”
 if (isNaN(process.argv[2])) {
 	console.log('Missing size');
-}
+} else {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
 	console.log('X'.repeat(parseInt(process.argv[2])));
+}
 }
