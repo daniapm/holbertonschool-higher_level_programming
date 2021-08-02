6601e3d040169c826bb0251605b0de73a4aebd7b
daniapm <daniajibellis@hotmail.com>
2021-08-02 14:36:46 -0500

    file that converted argumentos to integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ if (process.argv[2] === undefined) {
 } else if (isNaN(process.argv[2])) {
   console.log('Not a number');
 } else {
-  console.log('My number' + ' ' + parseInt(process.argv[2]));
+  console.log('My number:' + ' ' + parseInt(process.argv[2]));
 }
