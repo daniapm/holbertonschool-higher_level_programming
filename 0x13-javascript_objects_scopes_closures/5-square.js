f80bfbb9202e58111963e8c87ad8f0f8b7131eed
daniapm <daniajibellis@hotmail.com>
2021-08-03 15:33:53 -0500

    edit square class

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,29 +1,11 @@
 #!/usr/bin/node
 
-const Rectangle = require('./1-rectangle');
+const Rectangle = require('./4-rectangle');
 
 //  class Rectangle that defines a rectangle
 class Square extends Rectangle {
   constructor (size) {
-    super(size);
-    this.height = size;
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    const aux = this.width;
-    this.width = this.height;
-    this.height = aux;
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
+    super(size, size);
   }
 }
 module.exports = Square;
