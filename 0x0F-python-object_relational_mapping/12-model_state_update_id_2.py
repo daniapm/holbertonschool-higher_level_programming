cdd4237e03e041735b5ca48252d809086f8271e4
daniapm <daniajibellis@hotmail.com>
2021-08-08 12:19:59 -0500

    Update a state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+script that changes the name of a State object from
+the database hbtn_0e_6_usa
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    Session = Session()
+
+    query = Session.query(State).filter(State.id == 2).first()
+    query.name = 'New Mexico'
+
+    Session.commit()
+    Session.close()
