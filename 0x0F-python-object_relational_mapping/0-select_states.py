f969acc2d72df368fef59d9ed60c5c4490083f51
daniapm <daniajibellis@hotmail.com>
2021-08-08 11:21:36 -0500

    fix identation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 script that lists all states from the database hbtn_0e_0_usa
 """
@@ -10,9 +9,9 @@ if __name__ == '__main__':
 
     miConexion = MySQLdb.connect(host='localhost', user=sys.argv[1],
                                  passwd=sys.argv[2], db=sys.argv[3])
-cur = miConexion.cursor()
-cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-for name, id in cur.fetchall():
-    print((name, (id)))
-cur.close()
-miConexion.close()
+    cur = miConexion.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    for name, id in cur.fetchall():
+        print((name, (id)))
+    cur.close()
+    miConexion.close()
