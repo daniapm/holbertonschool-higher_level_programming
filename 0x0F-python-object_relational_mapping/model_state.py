00badfcebd5d40b405c8497095ce3e4e9fa45886
daniapm <daniajibellis@hotmail.com>
2021-08-08 11:54:24 -0500

    fix sintax

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,17 +3,17 @@
 python file that contains the class definition of a State
 and an instance Base = declarative_base()
 """
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-"""
-State Class inherits that Base
-"""
-
 
 class State(Base):
+    """
+    State class
+    """
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False)
