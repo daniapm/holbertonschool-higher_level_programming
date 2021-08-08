3ccfd4ba1b32baa4434873e9831185276592f4f1
daniapm <daniajibellis@hotmail.com>
2021-08-08 11:33:37 -0500

    use SQL Injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+"""
+script that takes in arguments and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument. But this time,
+write one that is safe from MySQL injections!
+"""
+
+if __name__ == '__main__':
+
+    miConexion = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                                 passwd=sys.argv[2], db=sys.argv[3])
+    cur = miConexion.cursor()
+    filter = "SELECT * FROM states\
+    WHERE states.name = %s ORDER BY states.id ASC"
+    cur.execute(filter, (sys.argv[4], ))
+    for name, id in cur.fetchall():
+        print((name, (id)))
+    cur.close()
+    miConexion.close()
