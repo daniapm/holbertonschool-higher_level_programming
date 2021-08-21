5ee39e5889ba14895425852635cb1785c1c09a7a
daniapm <daniajibellis@hotmail.com>
2021-08-20 21:54:44 -0500

    Error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8)
+"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        req = urllib.request.Request('sys.argv[1]')
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPrror as e:
+        print("Error code: {}".format(e.code))      
