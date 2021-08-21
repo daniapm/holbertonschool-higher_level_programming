57d8f5c40ecd21c84498fadf8a2646446ef51a76
daniapm <daniajibellis@hotmail.com>
2021-08-20 22:42:47 -0500

    fix code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
         response.raise_for_status()
         print(response.text)
     except:
-        print("Error code: {}".format(response.status_code()))
+        print("Error code: {}".format(response.status_code))
