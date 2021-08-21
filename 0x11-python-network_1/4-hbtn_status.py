aa8fcd781f1feb77290d4b6cf937142e9ab46f7e
daniapm <daniajibellis@hotmail.com>
2021-08-20 22:04:19 -0500

    fix pep8 styles

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,7 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    	response = requests.get('https://intranet.hbtn.io/status').text
+        response = requests.get('https://intranet.hbtn.io/status').text
         print("Body response:")
         print("\t- type: {}".format(type(response)))
         print("\t- content: {}".format(response))
