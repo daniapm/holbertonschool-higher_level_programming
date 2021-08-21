4d5fa90c274e30487b2455631bb7a0939367f59c
daniapm <daniajibellis@hotmail.com>
2021-08-20 22:03:52 -0500

    file that reques url

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://intranet.hbtn.io/status
+"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    	response = requests.get('https://intranet.hbtn.io/status').text
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
