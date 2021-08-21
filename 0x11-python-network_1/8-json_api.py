2a1b63845e805cd71816fe7a4da6cff8e76b1edc
daniapm <daniajibellis@hotmail.com>
2021-08-20 23:08:54 -0500

    fix pep8 styles

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL and an email address, sends a
-POST request to the passed URL with the email as a parameter,
-and finally displays the body of the response.
+Python script that takes in a letter and sends a POST
+request to http://0.0.0.0:5000/search_user with the letter
+as a parameter.
 """
 import requests
 import sys
@@ -10,13 +10,14 @@ import sys
 
 if __name__ == "__main__":
     params = ""
-    if len(sys.argv) == 2:
+    if (len(sys.argv) == 2):
         params = sys.argv[1]
     try:
-        response = requests.post("http://0.0.0.0:5000/search_user", data={q: params}).json()
+        response = requests.post("http://0.0.0.0:5000/search_user",
+                                 data={q: params}).json()
         if ("name" in respose) and ("id" in response):
-            print("[{}] {} ".format(response["id"], response["name"])
+            print("[{}] {} ".format(response["id"], response["name"]))
         else:
             print("No result")
-        except:
-            print("Not a valid JSON")
+    except:
+        print("Not a valid JSON")
