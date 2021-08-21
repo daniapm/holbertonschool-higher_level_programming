18553fe2756cd9613de22fe5a5c7be536c94efa0
daniapm <daniajibellis@hotmail.com>
2021-08-20 21:16:58 -0500

    fix pep8 style

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, sends a request to the URL and displays the value
-of the X-Request-Id variable found in the header of the response
+Python script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable found in
+the header of the response
 """
 import urllib.request
 import sys
@@ -9,4 +10,4 @@ import sys
 
 if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.headers["X-Request-Id"])
+        print(response.headers["X-Request-Id"])
