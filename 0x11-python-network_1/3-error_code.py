10178b1c124590d21f26628e18fbb16bf0708749
daniapm <daniajibellis@hotmail.com>
2021-08-20 21:55:17 -0500

    fix pep8 style

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
         with urllib.request.urlopen(req) as response:
             print(response.read().decode("utf-8"))
     except urllib.error.HTTPrror as e:
-        print("Error code: {}".format(e.code))      
+        print("Error code: {}".format(e.code))
