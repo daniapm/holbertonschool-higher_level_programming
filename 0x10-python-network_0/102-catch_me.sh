188fded318f999c9bda41343cda6cca66df4e982
daniapm <daniajibellis@hotmail.com>
2021-08-24 10:01:08 -0500

    fix code

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -s L X PUT -H "Origin: HolbertonSchool" -d  "user_id=98" 0.0.0.0:5000/catch_me
+curl -sLX PUT -d "Origin: HolbertonSchool" -H  "user_id=98" 0.0.0.0:5000/catch_me
