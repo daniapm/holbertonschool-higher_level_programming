164f5d7176d01f43d4c66be282ab93e52c15ffdd
daniapm <daniajibellis@hotmail.com>
2021-08-24 16:29:29 -0500

    fix code

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         response = requests.post("http://0.0.0.0:5000/search_user",
                                  data={'q': argv[1]})
     else:
-        response = requests.post(url, data={'q': ""})
+        response = requests.post("http://0.0.0.0:5000/search_user", data={'q': ""})
     try:
         dataj = response.json()
         if dataj:
