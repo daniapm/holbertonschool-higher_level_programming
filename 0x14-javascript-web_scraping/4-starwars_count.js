e218f716a313b5947e115b585f013ff356cea003
daniapm <daniajibellis@hotmail.com>
2021-09-14 16:57:26 -0500

    fix code

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // script that display the status code of a GET request.
 const request = require('request');
-request('https://swapi-api.hbtn.io/api/films/', function (error, response, body) {
+request(process.argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
   } else if (response.statusCode === 200) {
