999bb6c3a30e28e096ec8ffc2d30962bf12c6fbb
daniapm <daniajibellis@hotmail.com>
2021-09-14 15:04:09 -0500

    adding utf8

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 // script that writes a string to a file.
 const fs = require('fs');
 const data = process.argv[3];
-fs.writeFile(process.argv[2], data, (err) => {
+fs.writeFile(process.argv[2], 'utf-8', data, (err) => {
   if (err) {
     console.log(err);
   } else {
