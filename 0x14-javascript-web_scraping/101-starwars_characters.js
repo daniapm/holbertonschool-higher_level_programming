3848f43187231834851cd134034be398469e10bd
daniapm <daniajibellis@hotmail.com>
2021-09-14 21:03:58 -0500

    Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,7 +6,7 @@ request(('https://swapi-api.hbtn.io/api/films/' + process.argv[2]), function (er
     console.log(error);
   } else if (response.statusCode === 200) {
     const results = (JSON.parse(body).characters);
-    for (let i = 0; i < results.length; i++) {
+    for (let i = 0; i < results.length; ++i) {
       request(results[i], function (error, response, body) {
         if (error) {
           console.log(error);
