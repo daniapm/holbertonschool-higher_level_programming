9665b0ac9e49a89fbb0bf38b82d461941fbdcb48
daniapm <daniajibellis@hotmail.com>
2021-09-14 20:55:12 -0500

    script that prints all characters of a Star Wars movie

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// script that prints all characters of a Star Wars movie
+const request = require('request');
+request(('https://swapi-api.hbtn.io/api/films/' + process.argv[2]), function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const results = (JSON.parse(body).characters);
+    for (let i = 0; i < results.length; i++) {
+      request(results[i], function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else if (response.statusCode === 200) {
+          const results = (JSON.parse(body).name);
+          console.log(results);
+        }
+      });
+    }
+  }
+});
